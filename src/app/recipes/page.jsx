0e80ab846d56eaf6a6ab1899ef008c00992f2b78
dmcneill15 <dvveijeren@gmail.com>
2024-09-14 17:27:02 +1200

    recipe cards displaying

--- src/app/recipes/page.jsx
+++ src/app/recipes/page.jsx
@@ -1,6 +1,40 @@
-'use client' // client component, not server rendered
+import RecipeCard from '@/components/RecipeCard';
+//import recipesData from '@/data/recipes.js'
 
-export default function About() {
+export default function Recipes() {
+    const recipesData = [
+       {
+            recipe_id: 1,
+            recipe_title: 'Spag Bol',
+            method: 'Chop onions, carrots and celery. Fry until soft. Add the remaining ingredients and cook until rich and delicious. Cook pasta according to packet instructions. Serve with fresh basil and grated cheese.',
+            servings: 4,
+            image: '#',
+        },
+        {
+            recipe_id: 2,
+            recipe_title: 'Pizza',
+            method: 'The best pizza recipe here',
+            servings: 4,
+            image: '#',
+        },
+        {
+            recipe_id: 3,
+            recipe_title: 'Roast chicken',
+            method: 'Best recipe for roast chicken here',
+            servings: 4,
+            image: '#',
+        }
+    ]
+
+    let recipesJSX;
+    try{
+        console.log(JSON.stringify(recipesData));
+        recipesJSX = <RecipeCard recipes={recipesData}></RecipeCard>
+    }
+    catch(error){
+        recipesJSX = <span>Unable to Fetch Recipes</span>
+    }
+
     return (
         <main>
             <section>
@@ -8,6 +42,7 @@ export default function About() {
                     <h1 className="page-title">Your Recipe Catalog</h1>
                 </div>
             </section>
+            {recipesJSX}
         </main>
     )
 }
--- src/components/RecipeCard.jsx
+++ src/components/RecipeCard.jsx
@@ -1,43 +1,45 @@
 'use client' // client component, not server rendered
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Card, Col, Row, Container } from 'react-bootstrap';
-import Image from "next/image";
+import { Card, Col, Row, Container, CardFooter } from 'react-bootstrap';
 
 export default function Orders({ recipes }) {
     if (recipes.length === 0) {
         return (
-            <Container className='justify-content-center align-items-center'>
-                <Row xs={1} sm={2} md={2} className="g-4 justify-content-center">
-                    <Col className="g-4 justify-content-center">
-                        <Card className="border-light border-2">
-                            <Card.Body>
-                                <Card.Title className="h6"><p>No recipes added yet...</p></Card.Title>
-                            </Card.Body>
-                        </Card>
+            <Container className=' justify-content-center align-items-center'>
+                <Row xs={1} sm={1} md={1} className="g-4 justify-content-center">
+                    <Col className="center g-4 justify-content-center">
+                        <p className="h6"><p>No recipes added yet...</p></p>
                     </Col>
                 </Row>
-                <a className="custom-btn btn btn-outline-light" href="#" role="button"> +Add a Recipe</a>
+                <div className='center'>
+                    <a className="custom-btn btn btn-outline-light center" href="#" role="button"> +Add a Recipe</a>
+                </div>
             </Container>
         );
     }
 
     return (
         <Container className='justify-content-center align-items-center'>
-            {recipes.map(recipe => (
-                <Row key={recipe.recipe_id} xs={1} sm={2} md={2} className="g-4 justify-content-center">
-                    <Col key={recipe_id} className="g-4 justify-content-center">
-                        <Card className="border-light border-2 mb-3">
-                            <Image src={recipe.image} class="card-img-top" alt="..." />
+            <Row xs={1} sm={2} md={4} className="justify-content-center">
+                {recipes.map(recipe => (
+                    <Col key={recipe.recipe_id} className="g-3 justify-content-center">
+                        <Card className=' border-2 text-center'>
+                            <Card.Img className="mx-auto pt-2" src="#" />
                             <Card.Body>
-                                <Card.Title className="fw-bold h6"><p>{recipe.recipe_title}</p></Card.Title>
-                                <Card.Text>
-                                    Servings: {recipe.servings}<br></br>
-                                </Card.Text>
+                                <Card.Title className='fs-5'>{recipe.recipe_title}</Card.Title>
+                                <Card.Text>Servings: {recipe.servings}</Card.Text>
+                                <CardFooter className="">
+                                    <a className="custom-btn btn btn-outline-light" href="#" role="button">+Add to Plan</a>
+                                    <div>
+                                        <a className="custom-btn btn btn-outline-light" href="#" role="button">Edit</a>
+                                        <a className="custom-btn btn btn-outline-light" href="#" role="button">Delete</a>
+                                    </div>
+                                </CardFooter>
                             </Card.Body>
                         </Card>
                     </Col>
-                </Row>
-            ))}
+                ))}
+            </Row>
         </Container >
     )
 }
